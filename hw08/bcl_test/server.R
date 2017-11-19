680af4838b0d663f4748394534da641941c5a03c
Seth <seth.wynes@gmail.com>
2017-11-19 14:26:20 -0800

    country issue fixed

--- hw08/bcl_test/server.R
+++ hw08/bcl_test/server.R
@@ -1,12 +1,3 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(ggplot2)
 library(dplyr)
@@ -15,7 +6,13 @@ bcl <- read.csv("C:/Users/AsusW10/Documents/STAT545/STAT545-hw-wynes-chris/hw08/
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-   
+  
+  output$countryOutput <- renderUI({
+    selectInput("countryInput", "Country",
+                sort(unique(bcl$Country)),
+                selected = "CANADA")
+  })
+  
   output$coolplot <- renderPlot({
     
     filtered <-
--- hw08/bcl_test/ui.R
+++ hw08/bcl_test/ui.R
@@ -1,12 +1,3 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(ggplot2)
 bcl <- read.csv("C:/Users/AsusW10/Documents/STAT545/STAT545-hw-wynes-chris/hw08/bcl_test/bcl-data.csv", stringsAsFactors = FALSE)
@@ -21,13 +12,12 @@ shinyUI(fluidPage(
       radioButtons("typeInput", "Product type",
                    choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
                    selected = "WINE"),
-      selectInput("countryInput", "Country",
-                  choices = c("CANADA", "FRANCE", "ITALY"))
+      uiOutput("countryOutput")
     ),
     mainPanel(
       plotOutput("coolplot"),
-    br(), br(),
-    tableOutput("results"))
-    )
+      br(), br(),
+      tableOutput("results"))
   )
+)
 )
