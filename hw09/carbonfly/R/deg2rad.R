602076d2abf1f1547c8a34eebdcce3a31b25bad1
Seth <seth.wynes@gmail.com>
2017-11-23 15:53:33 -0800

    h

---
+++ hw09/carbonfly/R/deg2rad.R
@@ -0,0 +1,17 @@
+#' Change degrees into radians
+#'
+#' This function will take coordinates in degrees and change them into radians
+#' This is useful as our function that finds Greater Circle Distance requires radians
+#'
+#' @param deg The vector to be converted
+#'
+#' @return A vector that is radian version of \code{deg}.
+#'
+#' @details
+#' This function isn't complicated.
+#'
+#' @examples
+#' deg2rad(180)
+#' deg2rad(60)
+
+deg2rad <- function(deg) return(deg*pi/180) #Change degrees into radians
