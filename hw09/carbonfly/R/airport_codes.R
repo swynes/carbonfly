1f5f4e6f00cd25b5d8bd2042fc451fc10aa5a3f2
Seth <seth.wynes@gmail.com>
2017-11-23 17:24:05 -0800

    added datafile

---
+++ hw09/carbonfly/R/airport_codes.R
@@ -0,0 +1,9 @@
+#' Data for three letter airport code lookup
+#'
+#' This table contains a list of airports in three letter codes
+#' Each airport code has a corresponding longitude and latitude in degrees
+#'
+
+#' @examples
+#' the latitude of YYJ is 48.64694 and the longitude is negative 123.425833
+"airport_codes"
